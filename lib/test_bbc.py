daeb55e3d44b124fcf85d81e0694ca48337ed6f0
jk977 <jdk64@comcast.net>
2020-03-24 23:24:12 -0700

    Adding literals_length test to BBC tests

--- lib/test_bbc.py
+++ lib/test_bbc.py
@@ -50,14 +50,12 @@ class TestBBC(ut.TestCase):
             self.assertEqual(gap_length('00000000' * n), gap_max)
 
     def test_literals_length(self):
-        for i in range(100):
+        for i in range(8, 100):
             # test strings without literals
             zeroes = '0' * i
-            ones = '1' * i
             offs = ('00100000' + '00000100') * i
 
             self.assertEqual(literals_length(zeroes), 0)
-            self.assertEqual(literals_length(ones), 0)
             self.assertEqual(literals_length(offs), 0)
 
         a = '00000011'
@@ -71,9 +69,14 @@ class TestBBC(ut.TestCase):
             self.assertEqual(literals_length(literal), 1)
 
         for literal in a, b, c, d, e:
-            for i in range(100):
-                # test multi-byte literals
+            for i in range(16):
+                # test multi-byte literals under the literal limit
                 self.assertEqual(literals_length(literal * i), i)
+
+        for literal in a, b, c, d, e:
+            for i in range(16, 100):
+                # test multi-byte literals over the literal limit
+                self.assertEqual(literals_length(literal * i), 15)
 
     def test_dirty_bit_pos(self):
         pass
