65ff519782553df61612f40d2e91e74159eaa98c
jk977 <jdk64@comcast.net>
2020-03-24 21:19:13 -0700

    Improving test_wah.py documentation

--- lib/test_wah.py
+++ lib/test_wah.py
@@ -35,57 +35,68 @@ def compress(index, word_size):
 
 class TestWAH(ut.TestCase):
     def test_run_length(self):
-        for word_size in range(8, 65):
-            self.assertEqual(run_length('', word_size), 0)
-            self.assertEqual(run_length('0', word_size), 0)
-            self.assertEqual(run_length('1', word_size), 0)
-            self.assertEqual(run_length('01', word_size), 0)
-            self.assertEqual(run_length('10', word_size), 0)
-            self.assertEqual(run_length('01'*1000, word_size), 0)
+        for ws in range(8, 65):
+            # test for no runs
+            self.assertEqual(run_length('', ws), 0)
+            self.assertEqual(run_length('0', ws), 0)
+            self.assertEqual(run_length('1', ws), 0)
+            self.assertEqual(run_length('01', ws), 0)
+            self.assertEqual(run_length('10', ws), 0)
+            self.assertEqual(run_length('01'*1000, ws), 0)
 
-        for word_size in range(8, 65):
-            sect = word_size - 1
-            self.assertEqual(run_length('0'*sect, word_size), 1)
-            self.assertEqual(run_length('0'*sect*5, word_size), 5)
-            self.assertEqual(run_length('0'*sect*63, word_size), 63)
+        for ws in range(8, 65):
+            # test for runs that don't overflow
+            sect = ws - 1
+            self.assertEqual(run_length('0'*sect, ws), 1)
+            self.assertEqual(run_length('0'*sect*5, ws), 5)
+            self.assertEqual(run_length('0'*sect*63, ws), 63)
 
-        # overflow tests
-        self.assertEqual(run_length('0'*7*64, 8), 63)
-        self.assertEqual(run_length('0'*15*16384, 16), 16383)
+        # test for overflows
+        self.assertEqual(run_length('0000000'*64, 8), 63)
+        self.assertEqual(run_length('000000000000000'*16384, 16), 16383)
 
     def test_encode_run(self):
+        # test for single-word runs without leftovers
+        self.assertEqual(encode_run('111', 4), ('', '1101'))
+        self.assertEqual(encode_run('0000000', 8), ('', '10000001'))
+
+        # test for single-word runs with leftovers
         self.assertEqual(encode_run('111110', 4), ('110', '1101'))
-        self.assertEqual(encode_run('111111', 4), ('', '1'*3 + '0'))
-        self.assertEqual(encode_run('111111111', 4), ('', '1'*4))
-
         self.assertEqual(encode_run('1111101', 4), ('1101', '1101'))
-        self.assertEqual(encode_run('1111110', 4), ('0', '1110'))
-        self.assertEqual(encode_run('1111111110', 4), ('0', '1'*4))
-
+        self.assertEqual(encode_run('0000000000', 8), ('000', '10000001'))
         self.assertEqual(encode_run('00000001111111', 8),
                          ('1111111', '10000001'))
 
-        self.assertEqual(encode_run('0'*7, 8), ('', '10000001'))
-        self.assertEqual(encode_run('0'*10, 8), ('000', '10000001'))
-        self.assertEqual(encode_run('0'*14, 8), ('', '10000010'))
-        self.assertEqual(encode_run('1'*7*(2**6 - 1), 8), ('', '1'*8))
-        self.assertEqual(encode_run('0'*7*2**6, 8), ('0'*7, '10' + '1'*6))
-        self.assertEqual(encode_run('0'*7*(2**6 - 1) + '10101', 8),
-                         ('10101', '10111111'))
+        # test for multi-word runs without leftovers
+        self.assertEqual(encode_run('111111', 4), ('', '1110'))
+        self.assertEqual(encode_run('111111111', 4), ('', '1111'))
+
+        self.assertEqual(encode_run('00000000000000', 8), ('', '10000010'))
+        self.assertEqual(encode_run('1111111'*(2**6 - 1), 8), ('', '11111111'))
 
         self.assertEqual(encode_run('0'*31*2**6, 32),
-                         ('', '1' + '0'*24 + '1000000'))
+                         ('', '10' + '0'*23 + '1000000'))
         self.assertEqual(encode_run('0'*31*2**7, 32),
-                         ('', '1' + '0'*23 + '10000000'))
+                         ('', '10' + '0'*22 + '10000000'))
+
+        # test for multi-word runs with leftovers
+        self.assertEqual(encode_run('1111110', 4), ('0', '1110'))
+        self.assertEqual(encode_run('1111111110', 4), ('0', '1111'))
+        self.assertEqual(encode_run('0000000'*2**6, 8),
+                         ('0000000', '10111111'))
+        self.assertEqual(encode_run('0000000'*(2**6 - 1) + '10101', 8),
+                         ('10101', '10111111'))
 
     def test_encode_literal(self):
         for i in range(3, 65):
+            # test zero-padding on short literals
             self.assertEqual(encode_literal('', i), ('', '0'*i))
             self.assertEqual(encode_literal('1', i), ('', '01' + '0'*(i - 2)))
             self.assertEqual(encode_literal('10', i), ('', '01' + '0'*(i - 2)))
             self.assertEqual(encode_literal('01', i),
                              ('', '001' + '0'*(i - 3)))
 
+        # test single-literal compression with 4-bit words
         self.assertEqual(encode_literal('001', 4), ('', '0001'))
         self.assertEqual(encode_literal('0101', 4), ('1', '0010'))
         self.assertEqual(encode_literal('01111', 4), ('11', '0011'))
@@ -94,6 +105,7 @@ class TestWAH(ut.TestCase):
         self.assertEqual(encode_literal('11010001', 4), ('10001', '0110'))
         self.assertEqual(encode_literal('111100001', 4), ('100001', '0111'))
 
+        # test single-literal compression with 8-bit words
         self.assertEqual(encode_literal('1110111', 8), ('', '01110111'))
         self.assertEqual(encode_literal('01010101', 8), ('1', '00101010'))
         self.assertEqual(encode_literal('110100110', 8), ('10', '01101001'))
@@ -110,21 +122,24 @@ class TestWAH(ut.TestCase):
         Test ``WAH.compress()`` on strings of literal bytes
         '''
 
-        for i in range(3, 65):
-            self.assertEqual(compress('', i), '')
-            self.assertEqual(compress('0', i), '0'*i)
-            self.assertEqual(compress('1', i), '01' + '0'*(i - 2))
+        for ws in range(3, 65):
+            # test literal zero-padding on short inputs
+            self.assertEqual(compress('', ws), '')
+            self.assertEqual(compress('0', ws), '0'*ws)
+            self.assertEqual(compress('1', ws), '01' + '0'*(ws - 2))
 
-        for word_size in range(4, 65):
-            sect_size = word_size - 1
+        for ws in range(4, 65):
+            sect_size = ws - 1
 
-            for lit_length in range(1, sect_size):
-                zeroes = '0' * (sect_size - lit_length)
-                ones = '1' * lit_length
+            for one_count in range(1, sect_size):
+                zeroes = '0' * (sect_size - one_count)
+                ones = '1' * one_count
 
                 for bits in it.combinations(it.chain(zeroes, ones), sect_size):
+                    # get a string of bits with the given word size in the
+                    # format "0+1+" such that there is not a run of either bit
                     word = ''.join(bits)
-                    result = compress(word, word_size)
+                    result = compress(word, ws)
                     self.assertEqual(result, '0' + word)
 
     def test_compress_runs(self):
@@ -137,6 +152,8 @@ class TestWAH(ut.TestCase):
             result_fmt = '{{:0{}b}}'.format(sect_size - 1)
 
             for run_length in range(1, 40):
+                # test the compression of a run of both zeroes and ones
+                # with run_length sections
                 zeroes = '0' * sect_size * run_length
                 ones = '1' * sect_size * run_length
 
